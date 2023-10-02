6e3e489c4a85436087c0987341954f907becad0c
SimaVitalay <simachev28@gmail.com>
2023-10-02 11:38:36 +0300

    update2

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const puppeteer = require('puppeteer');
-const cors = require('cors');
-
 const app = express();
 // const port = 3000;
 // const port = process.env.PORT || 5000;
-var serverPort = 8080;
+const express = require('express');
+const puppeteer = require('puppeteer');
+const cors = require('cors');
+var serverPort =3000;
 var port = process.env.PORT || serverPort;
 app.use(cors()); // Enable CORS for all routes
 
@@ -14,7 +13,8 @@ app.get('/car/:vin', async (req, res) => {
 
   const browser = await puppeteer.launch({
     headless: 'new', // Set to true for production use
-    // args: ['--no-sandbox', '--disable-setuid-sandbox'] // Required for running in Docker
+    executablePath: '/usr/bin/chromium-browser',
+     args: ['--no-sandbox', '--disable-setuid-sandbox'], // Required for running in Docker
     waitUntil: 'networkidle0'
   });
   const page = await browser.newPage();
